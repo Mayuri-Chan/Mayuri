fa1aa5dc1cb76c932fd203cb1dc32684030438d7
wulan17 <galihgustip@gmail.com>
2020-10-31 16:53:19 +0700

    Mayuri: Allow sudoers to execute alive
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- mayuri/modules/alive.py
+++ mayuri/modules/alive.py
@@ -1,5 +1,6 @@
 import sys
 from mayuri import Command, OWNER, AddHandler
+from mayuri.modules.sudo import sudo
 from pyrogram import filters, __version__
 
 __MODULE__ = "Alive"
@@ -10,8 +11,9 @@ Module ini digunakan untuk menampilkan status bot.
 > `/on`
 """
 
+@sudo
 async def alive(client, message):
 	alive_text = "Bot services is running...\n----------------------------------\n•  ⚙️ Pyrogram    : v{}\n•  🐍 Python         : v{}\n----------------------------------".format(__version__,sys.version.split(' ')[0])
 	await message.reply_text(alive_text)
 
-AddHandler(alive,(filters.command("alive", Command) | filters.command("on", Command)) & filters.user(OWNER))
+AddHandler(alive,(filters.command("alive", Command) | filters.command("on", Command)))
