7c2a30036846099364ea15649447e3c42b6d1208
Galih Gusti Priatna <galihgustip@gmail.com>
2021-05-22 23:05:20 +0700

    Mayuri: Update sed modules
    from https://github.com/TeamDerUntergang/Telegram-SedenUserBot/blob/seden/sedenbot/modules/sed.py

--- mayuri/modules/sed.py
+++ mayuri/modules/sed.py
@@ -1,6 +1,8 @@
 from mayuri import AddHandler
 from mayuri.modules.disableable import disableable
 from pyrogram import filters
+from re import IGNORECASE, I, match, sub
+from sre_constants import error as sre_err
 
 __MODULE__ = "Sed"
 __HELP__ = """
@@ -12,13 +14,91 @@ contoh :
 Mengubah semua kata 'anu' menjadi 'nganu' didalam pesan yang dibalas
 """
 
+DELIMITERS = ("/", ":", "|", "_")
+
+def separate_sed(sed_string):
+	if (
+		len(sed_string) > 2
+		and sed_string[1] in DELIMITERS
+		and sed_string.count(sed_string[1]) >= 2
+	):
+		delim = sed_string[1]
+		start = counter = 2
+		while counter < len(sed_string):
+			if sed_string[counter] == '\\':
+				counter += 1
+
+			elif sed_string[counter] == delim:
+				replace = sed_string[start:counter]
+				counter += 1
+				start = counter
+				break
+
+			counter += 1
+
+		else:
+			return None
+
+		while counter < len(sed_string):
+			if (
+				sed_string[counter] == '\\'
+				and counter + 1 < len(sed_string)
+				and sed_string[counter + 1] == delim
+			):
+				sed_string = sed_string[:counter] + sed_string[counter + 1 :]
+
+			elif sed_string[counter] == delim:
+				replace_with = sed_string[start:counter]
+				counter += 1
+				break
+
+			counter += 1
+		else:
+			return replace, sed_string[start:], ''
+
+		flags = ''
+		if counter < len(sed_string):
+			flags = sed_string[counter:]
+		return replace, replace_with, flags.lower()
+	return None
+
+
 @disableable
 async def sed(client,message):
-	text = (message.text).split('/')
-	old_word = text[1]
-	new_word = text[2]
-	old_text = message.reply_to_message.text
-	new_text = old_text.replace(old_word,new_word)
-	await message.reply_to_message.reply_text(new_text)
+	"""For sed command, use sed on Telegram."""
+	sed_result = separate_sed(message.text or message.caption)
+	textx = message.reply_to_message
+	if sed_result:
+		if textx:
+			to_fix = textx.text
+		else:
+			return await message.reply_text(
+				"`Master, I don't have brains. Well you too don't I guess.`"
+			)
+
+		repl, repl_with, flags = sed_result
+
+		if not repl:
+			return await message.reply_text(
+				"`Master, I don't have brains. Well you too don't I guess.`"
+			)
+
+		try:
+			check = match(repl, to_fix, flags=IGNORECASE)
+			if check and check.group(0).lower() == to_fix.lower():
+				return await message.reply_text("`Boi!, that's a reply. Don't use sed`")
+
+			if "i" in flags and "g" in flags:
+				text = sub(repl, repl_with, to_fix, flags=I).strip()
+			elif "i" in flags:
+				text = sub(repl, repl_with, to_fix, count=1, flags=I).strip()
+			elif "g" in flags:
+				text = sub(repl, repl_with, to_fix).strip()
+			else:
+				text = sub(repl, repl_with, to_fix, count=1).strip()
+		except sre_err:
+			return await message.reply_text("B O I! [Learn Regex](https://regexone.com)")
+		if text:
+			await message.reply_text(text)
 
 AddHandler(sed,filters.reply & filters.regex('^s/(.*?)'))
