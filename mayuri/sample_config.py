4902560ede8ea2023bb220aa7704d51a325b21ae
月 <wulan17@nusantararom.org>
2022-08-13 00:03:01 +0700

    Update sample_config.py

--- mayuri/sample_config.py
+++ mayuri/sample_config.py
@@ -2,9 +2,9 @@ class Config(object):
 	API_ID = "" # from my.telegram.org
 	API_HASH = "" # from my.telegram.org
 	BOT_TOKEN = "" # bot token from https://t.me/BotFather
-	OWNER = 864 # Your telegram id, you can set more than one, use comma (,) for separator
+	OWNER = 864 # Your telegram id
 	DATABASE_URL = "postgresql://postgres:password@localhost:5172/db" # Your database URI
 	WORKERS = 6
 	CUSTOM_PREFIXS = ['/', '$']
-	LOG_CHAT = -1001233232
-	USE_OCR = False
+	LOG_CHAT = -1001233232 # required for kang sticker
+	USE_OCR = False # you need to install tesseract on your server to enable ocr
